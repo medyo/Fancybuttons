b4fd010a370d709097b53cbe8dd1ea3017877698
medyo <elmehdi.sakout@gmail.com>
2015-09-26 01:04:40 +0100

    better naming convention + remove unused converters

--- fancybuttons_library/src/main/java/mehdi/sakout/fancybuttons/Utils.java
+++ fancybuttons_library/src/main/java/mehdi/sakout/fancybuttons/Utils.java
@@ -4,19 +4,12 @@ import android.content.Context;
 
 public class Utils {
 
-	public static int PxToSp(final Context context, final float px) {
+	public static int pxToSp(final Context context, final float px) {
 		return Math.round(px / context.getResources().getDisplayMetrics().scaledDensity);
 	}
 
-	public static int SptoPx(final Context context, final float sp) {
+	public static int spToPx(final Context context, final float sp) {
 		return Math.round(sp * context.getResources().getDisplayMetrics().scaledDensity);
 	}
 
-	public static int PxToDp(final Context context, final float px) {
-		return Math.round(px / context.getResources().getDisplayMetrics().density);
-	}
-
-	public static int DptoPx(final Context context, final float dp) {
-		return Math.round(dp * context.getResources().getDisplayMetrics().density);
-	}
 }
