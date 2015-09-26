a054c817cafd7aef06b15477d5cd07a211fe4ea3
medyo <elmehdi.sakout@gmail.com>
2015-09-26 01:05:32 +0100

    upgrade to AppCompatActivity

--- samples/src/main/java/mehdi/sakout/fancybuttons/samples/XmlButtons.java
+++ samples/src/main/java/mehdi/sakout/fancybuttons/samples/XmlButtons.java
@@ -2,11 +2,12 @@ package mehdi.sakout.fancybuttons.samples;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class XmlButtons extends ActionBarActivity {
+public class XmlButtons extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
