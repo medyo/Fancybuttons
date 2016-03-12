fa80eb39fbb471264b9fca84543c6a24986ef3cf
medyo <elmehdi.sakout@gmail.com>
2016-03-12 14:54:09 +0000

    new function to find a font inside assets

--- fancybuttons_library/src/main/java/mehdi/sakout/fancybuttons/Utils.java
+++ fancybuttons_library/src/main/java/mehdi/sakout/fancybuttons/Utils.java
@@ -1,6 +1,10 @@
 package mehdi.sakout.fancybuttons;
 
 import android.content.Context;
+import android.content.res.AssetManager;
+import android.graphics.Typeface;
+
+import java.util.Arrays;
 
 public class Utils {
 
@@ -10,6 +14,23 @@ public class Utils {
 
 	public static int spToPx(final Context context, final float sp) {
 		return Math.round(sp * context.getResources().getDisplayMetrics().scaledDensity);
+	}
+
+	public static Typeface findFont(Context context, String fontName, String defaultFont){
+		try{
+			AssetManager assets = context.getResources().getAssets();
+			if (Arrays.asList(assets.list("")).contains(fontName)){
+				return Typeface.createFromAsset(context.getAssets(), String.format("%s",fontName));
+			}else if (Arrays.asList(assets.list("fonts")).contains(fontName)){
+				return Typeface.createFromAsset(context.getAssets(), String.format("fonts/%s",fontName));
+			}else if (Arrays.asList(assets.list("iconfonts")).contains(fontName)){
+				return Typeface.createFromAsset(context.getAssets(), String.format("iconfonts/%s",fontName));
+			}else{
+				return Typeface.createFromAsset(context.getAssets(), String.format("%s", defaultFont));
+			}
+		}catch (Exception e){
+			return Typeface.createFromAsset(context.getAssets(), String.format("%s", defaultFont));
+		}
 	}
 
 }
