546b3d02588f868be4731db949fd5dd66ce7d5ee
medyo <elmehdi.sakout@gmail.com>
2016-05-02 22:56:21 +0100

    implement cache for fonts

--- fancybuttons_library/src/main/java/mehdi/sakout/fancybuttons/Utils.java
+++ fancybuttons_library/src/main/java/mehdi/sakout/fancybuttons/Utils.java
@@ -3,10 +3,15 @@ package mehdi.sakout.fancybuttons;
 import android.content.Context;
 import android.content.res.AssetManager;
 import android.graphics.Typeface;
+import android.text.TextUtils;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Utils {
+
+	private static Map<String, Typeface> cachedFontMap = new HashMap<String, Typeface>();
 
 	public static int pxToSp(final Context context, final float px) {
 		return Math.round(px / context.getResources().getDisplayMetrics().scaledDensity);
@@ -17,20 +22,38 @@ public class Utils {
 	}
 
 	public static Typeface findFont(Context context, String fontName, String defaultFont){
-		try{
-			AssetManager assets = context.getResources().getAssets();
-			if (Arrays.asList(assets.list("")).contains(fontName)){
-				return Typeface.createFromAsset(context.getAssets(), String.format("%s",fontName));
-			}else if (Arrays.asList(assets.list("fonts")).contains(fontName)){
-				return Typeface.createFromAsset(context.getAssets(), String.format("fonts/%s",fontName));
-			}else if (Arrays.asList(assets.list("iconfonts")).contains(fontName)){
-				return Typeface.createFromAsset(context.getAssets(), String.format("iconfonts/%s",fontName));
-			}else{
-				return Typeface.createFromAsset(context.getAssets(), String.format("%s", defaultFont));
+		if (cachedFontMap.containsKey(fontName)){
+			return cachedFontMap.get(fontName);
+		}else{
+			try{
+				AssetManager assets = context.getResources().getAssets();
+				if (Arrays.asList(assets.list("")).contains(fontName)){
+					Typeface typeface = Typeface.createFromAsset(context.getAssets(), String.format("%s",fontName));
+					cachedFontMap.put(fontName, typeface);
+					return typeface;
+				}else if (Arrays.asList(assets.list("fonts")).contains(fontName)){
+					Typeface typeface = Typeface.createFromAsset(context.getAssets(), String.format("fonts/%s",fontName));
+					cachedFontMap.put(fontName, typeface);
+					return typeface;
+				}else if (Arrays.asList(assets.list("iconfonts")).contains(fontName)){
+					Typeface typeface = Typeface.createFromAsset(context.getAssets(), String.format("iconfonts/%s",fontName));
+					cachedFontMap.put(fontName, typeface);
+					return typeface;
+				}else{
+					throw new Exception("Font not Found");
+
+				}
+			}catch (Exception e){
+				if (!TextUtils.isEmpty(defaultFont)){
+					Typeface typeface = Typeface.createFromAsset(context.getAssets(), String.format("%s", defaultFont));
+					cachedFontMap.put(fontName, typeface);
+					return typeface;
+				}else{
+					return Typeface.DEFAULT;
+				}
 			}
-		}catch (Exception e){
-			return Typeface.createFromAsset(context.getAssets(), String.format("%s", defaultFont));
 		}
+
 	}
 
 }
