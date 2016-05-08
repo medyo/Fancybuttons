7767352e45c217d5b574cd3c4b443c769d7a558a
medyo <elmehdi.sakout@gmail.com>
2016-05-08 23:11:41 +0100

    fix bug custom fonts bug

--- fancybuttons_library/src/main/java/mehdi/sakout/fancybuttons/Utils.java
+++ fancybuttons_library/src/main/java/mehdi/sakout/fancybuttons/Utils.java
@@ -4,7 +4,7 @@ import android.content.Context;
 import android.content.res.AssetManager;
 import android.graphics.Typeface;
 import android.text.TextUtils;
-
+import java.io.File;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
@@ -21,14 +21,22 @@ public class Utils {
 		return Math.round(sp * context.getResources().getDisplayMetrics().scaledDensity);
 	}
 
-	public static Typeface findFont(Context context, String fontName, String defaultFont){
+	public static Typeface findFont(Context context, String fonPath, String defaultFontPath){
+
+		String fontName = new File(fonPath).getName();
+		String defaultFontName = "";
+		if (!TextUtils.isEmpty(defaultFontPath)){
+			defaultFontName = new File(defaultFontPath).getName();
+		}
+
 		if (cachedFontMap.containsKey(fontName)){
 			return cachedFontMap.get(fontName);
 		}else{
 			try{
 				AssetManager assets = context.getResources().getAssets();
-				if (Arrays.asList(assets.list("")).contains(fontName)){
-					Typeface typeface = Typeface.createFromAsset(context.getAssets(), String.format("%s",fontName));
+
+				if (Arrays.asList(assets.list("")).contains(fonPath)){
+					Typeface typeface = Typeface.createFromAsset(context.getAssets(), fontName);
 					cachedFontMap.put(fontName, typeface);
 					return typeface;
 				}else if (Arrays.asList(assets.list("fonts")).contains(fontName)){
@@ -39,18 +47,16 @@ public class Utils {
 					Typeface typeface = Typeface.createFromAsset(context.getAssets(), String.format("iconfonts/%s",fontName));
 					cachedFontMap.put(fontName, typeface);
 					return typeface;
-				}else{
-					throw new Exception("Font not Found");
-
-				}
-			}catch (Exception e){
-				if (!TextUtils.isEmpty(defaultFont)){
-					Typeface typeface = Typeface.createFromAsset(context.getAssets(), String.format("%s", defaultFont));
-					cachedFontMap.put(fontName, typeface);
+				}else if (!TextUtils.isEmpty(defaultFontPath) && Arrays.asList(assets.list("")).contains(defaultFontPath)){
+					Typeface typeface = Typeface.createFromAsset(context.getAssets(), defaultFontPath);
+					cachedFontMap.put(defaultFontName, typeface);
 					return typeface;
-				}else{
-					return Typeface.DEFAULT;
+				} else {
+					throw new Exception("Font not Found");
 				}
+
+			}catch (Exception e){
+				return Typeface.DEFAULT;
 			}
 		}
 
