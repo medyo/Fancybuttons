7ce9494f3800b643cae439b0631245fdc40ad75e
Esa Firman <esafirm21@gmail.com>
2018-02-11 10:58:03 +0700

    Support text style

--- samples/src/main/java/mehdi/sakout/fancybuttons/samples/XmlButtons.java
+++ samples/src/main/java/mehdi/sakout/fancybuttons/samples/XmlButtons.java
@@ -1,10 +1,11 @@
 package mehdi.sakout.fancybuttons.samples;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 
 public class XmlButtons extends AppCompatActivity {
@@ -13,12 +14,20 @@ public class XmlButtons extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_xml_buttons);
+
+        findViewById(R.id.btComments).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(view.getContext(), "Button clicked", Toast.LENGTH_SHORT)
+                        .show();
+            }
+        });
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        
+
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.xml_buttons, menu);
         return true;
